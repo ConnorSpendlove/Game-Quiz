603c75841a84bec8f81e74bb3ce382b1c6158f24
Connor Spendlove <Connor.Spendlove44@gmail.com>
2024-01-24 01:42:05 -0700

    made question order random

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,76 @@
+var startButton = document.getElementById('start-btn')
+var questionsContainer = document.getElementById('questions-card')
+var questionElement = document.getElementById('question')
+var answerButton = document.getElementById('btn')
+var shuffledQuestions, currentQuestionIndex
 
+startButton.addEventListener('click', startGame)
 
-function seeNextQuestion () {
+function startGame () {
+console.log('Started');
+startButton.classList.add('hide')
+questionsContainer.classList.remove('hide')
+shuffledQuestions = questions.sort(() => Math.random() - .5)
+currentQuestionIndex = 0
+nextQuestion();
+}
+
+// answerButton.addEventListener('click', nextQuestion)
+
+function nextQuestion(){
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+    
+}
+
+function showQuestion (questions) {
+    questionElement.innerText = questions.question 
 
 }
 
-
-function keepScore () {
-
-
-}
-
-
-function showLeaderBoard() {
-
-}
+var questions = [
+    {
+        question: 'Question 1',
+        answers: [
+            {text: 'false', correct: false},
+            {text: 'false', correct: false},
+            {text: 'true', correct: true},
+            {text: 'false', correct: false},
+        ]
+    },
+    {
+        question: 'Question 2',
+        answers: [
+            {text: 'false', correct: false},
+            {text: 'true', correct: true},
+            {text: 'false', correct: false},
+            {text: 'false', correct: false},
+        ]
+    },
+    {
+        question: 'Question 3',
+        answers: [
+            {text: 'false', correct: false},
+            {text: 'false', correct: false},
+            {text: 'false', correct: false},
+            {text: 'true', correct: true},
+        ]
+    },
+    {
+        question: 'Question 4',
+        answers: [
+            {text: 'true', correct: true},
+            {text: 'false', correct: false},
+            {text: 'false', correct: false},
+            {text: 'false', correct: false},
+        ]
+    },
+    {
+        question: 'Question 5',
+        answers: [
+            {text: 'false', correct: false},
+            {text: 'true', correct: true},
+            {text: 'false', correct: false},
+            {text: 'false', correct: false},
+        ]
+    },
+]
